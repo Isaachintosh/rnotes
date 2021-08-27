014f7d3876ed3585e824c605e73e37006a4ebae5
isaachintosh <isaac.tuning@gmail.com>
2021-08-26 22:03:50 -0300

    class Component ListOfNotes has been created

---
+++ rnotes/src/components/listOfNotes.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class ListOfNotes extends Component {
+    render(){
+        return (
+            <ul>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Title</h3>
+                        </header>
+                        <p></p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Title</h3>
+                        </header>
+                        <p></p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                        <header>
+                            <h3>Title</h3>
+                        </header>
+                        <p></p>
+                    </section>
+                </li>
+            </ul>
+        );
+    }
+}
+
