61db1390b90721fe71a49d8f909234a590dfa0d6
isaachintosh <isaac.tuning@gmail.com>
2021-08-30 18:38:56 -0300

    delete method has been add

--- rnotes/src/components/cardNote/cardNote.jsx
+++ rnotes/src/components/cardNote/cardNote.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './style.css'
+import { ReactComponent as DeleteSVG } from '../../assets/img/delete.svg'
 
-class CardNote extends Component {
+class CardNote extends Component {    
+    eraseNote(){
+        const index = this.props.index
+        this.props.deleteNote(index)
+    }
+    
     render() { 
         return (
             <section className="card-note">
                 <header className="card-note_header">
                     <h3 className="card-note_title">{this.props.title}</h3>
+                    <div>
+                        <DeleteSVG
+                            className="btn-delete"
+                            onClick={this.eraseNote.bind(this)}
+                        />
+                    </div>
                 </header>
                 <p className="card-note_text">{this.props.text}</p>
+
             </section>
         );
     }
