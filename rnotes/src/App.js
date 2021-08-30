ad2924cbeec2a897ab93e0e7f0bcf40ec346934e
isaachintosh <isaac.tuning@gmail.com>
2021-08-30 18:36:28 -0300

    delete notes method has been created

--- rnotes/src/App.js
+++ rnotes/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import FormCreateNote from './components/formCreateNote';
 import ListOfNotes from './components/listOfNotes';
-import './App.css'
-import './index.css'
+import './assets/App.css'
+import './assets/index.css'
 
 class App extends Component {
 
@@ -20,11 +20,24 @@ class App extends Component {
     }
     this.setState(newState)
   }
+  _deleteNote(index, e){
+    if(window.confirm('Are you sure to delete this note?')){
+      let arrayOfNotes = this.state.notes
+      arrayOfNotes.splice(index, 1)
+      this.setState({notes: arrayOfNotes})
+    } else {
+      return
+    }
+  }
+
   render() {
     return (
       <section className="content">
         <FormCreateNote createNote={this.createNote.bind(this)}/>
-        <ListOfNotes notes={this.state.notes}/>
+        <ListOfNotes
+          deleteNote={this._deleteNote.bind(this)}
+          notes={this.state.notes}
+        />
       </section>
     );
   }
